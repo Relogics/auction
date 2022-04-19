c09acd464dc3ddc88fb5269d723d5ad9c9e9b66e
Binit Agarwal <rockingbinitagarwal@gmail.com>
2022-04-19 22:40:44 +0530

    artisan command created with command:update_status

--- app/Console/Commands/update_status.php
+++ app/Console/Commands/update_status.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use \App\Models\Auction;
 use \App\Models\Bid;
+use \App\Models\UserAttributesData;
 
 class update_status extends Command
 {
@@ -37,6 +38,57 @@ class update_status extends Command
      *
      * @return int
      */
+
+    public function send_msg($mobile_num,$msg)
+        {
+                if($mobile_num)
+                {
+                        $sms_message = "Hi! Your ARYA verification code is " . $otpcode;
+                        
+                        try {
+                        $curl = curl_init();
+                        curl_setopt_array($curl, array(
+                        CURLOPT_URL => "https://alerts.cbis.in/SMSApi/send",
+                        CURLOPT_RETURNTRANSFER => true,
+                        CURLOPT_ENCODING => "",
+                        CURLOPT_MAXREDIRS => 10,
+                        CURLOPT_TIMEOUT => 30,
+                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                        CURLOPT_CUSTOMREQUEST => "POST",
+                        CURLOPT_POSTFIELDS => "userid=aryacma&password=fqGnYXMS&mobile=".$mobile_num."&msg=".$msg."&senderid=ARYACM&msgType=text&dltEntityId=&dltTemplateId=&duplicatecheck=true&output=json&sendMethod=quick",
+                        CURLOPT_HTTPHEADER => array(
+                        "apikey: somerandomuniquekey",
+                        "cache-control: no-cache",
+                        "content-type: application/x-www-form-urlencoded"
+                        ),
+                        ));
+
+                        $response = curl_exec($curl);
+                        $err = curl_error($curl);
+
+                        curl_close($curl);
+
+                        if ($err) {
+                        //echo "cURL Error #:" . $err;
+                                return $this->respondWithErrorValidation(trans("messages.An_error_occurred"));
+                        } else {
+                        //echo '<pre>';print_r($response);
+                                return $this->respondWithoutPagination(trans("messages.Verification_code_sent"));
+                        }
+                        } catch (Exception $e) {
+
+                        //echo 'Message:' . $e->getMessage();
+                        }
+                        /*
+                        event(new OtpRegisterEvent($post['mobile'], $otpcode));
+                        return $this->respondWithoutPagination(trans("messages.Verification_code_sent"));
+                        */
+                }
+                else {
+                        return $this->respondWithError(trans("messages.An_error_occurred"));
+                }
+        }
+
     public function handle()
     {   
         $auction=Auction::where('auction_end_date_time','<',date('Y-m-d H:i:s'))->get();
@@ -46,33 +98,51 @@ class update_status extends Command
             
             //if auction type is english
             if($auction_data->auction_type=='english'){
-                $max_bid = Bid::where('auction_id',$auction_data->id)->orderBy('bid_price','desc')->orderBy('bid_time','asc')->first();
+                $max_bid = Bid::where('auction_id',$auction->id)->orderBy('bid_price','desc')->orderBy('bid_time','asc')->first();
                 if($max_bid){
                     $max_bid->status='won';
                     $max_bid->save();
-                    echo "Auction ID: ".$auction_data->id." has been won by ".$max_bid->user_id."\n";   //replace with sms topic
+                    $user_data = UserAttributeData::where('user_id',$max_bid->user_id)->first();
+                    $mobile_num = $user_data->mobile;
+                    $msg = "Dear User, auction".$auction->auction_id." has ended. You have won the auction. Visit ARYAMKP site for more details. -ARYAMKP";
+                    $this->send_msg($mobile_num,$msg);
                 }
-                $remaining_bid = Bid::where('auction_id',$auction_data->id)->where('bid_id','<>',$max_bid->bid_id)->get();
+                $remaining_bid = Bid::where('auction_id',$auction->id)->where('bid_id','<>',$max_bid->bid_id)->get();
                 foreach($remaining_bid as $bid){
                     $bid->status='lost';
                     $bid->save();
-                    echo "Auction ID: ".$auction_data->id." has been lost by ".$bid->user_id."\n"; //replace with sms topic
+                    $user_data = UserAttributeData::where('user_id',$max_bid->user_id)->first();
+                    $mobile_num = $user_data->mobile;
+                    $msg = "Dear User, auction - ".$auction->auction_id." has ended. You have lost the auction. -ARYAMKP";
+                    $this->send_msg($mobile_num,$msg);
                 }
-            }
+        }
             //if auction type is yankee
-            //logic to be changed based on what to do with qty
             elseif($auction_data->auction_type=='yankee'){
-                $max_bid = Bid::where('auction_id',$auction_data->id)->orderBy('bid_price','desc')->orderBy('bid_time','asc')->first();
+                $max_bid = Bid::where('auction_id',$auction->id)->orderBy('bid_qty','desc')->orderBy('bid_time','asc')->first();
+                $rem_quant = $auction_data->auction_quantity - $max_bid->bid_qty;
                 if($max_bid){
                     $max_bid->status='won';
                     $max_bid->save();
-                    echo "Auction ID: ".$auction_data->id." has been won by ".$max_bid->user_id."\n";   //replace with sms topic
+                    $user_data = UserAttributeData::where('user_id',$max_bid->user_id)->first();
+                    $mobile_num = $user_data->mobile;
+                    $msg = "Dear User, you were declared winner in auction ".$auction->auction_id." Click here to contact Seller ".$auction->seller_name." for further steps.";
+                    $this->send_msg($mobile_num,$msg);
                 }
-                $remaining_bid = Bid::where('auction_id',$auction_data->id)->where('bid_price','<>',$max_bid->bid_price)->get();
-                foreach($remaining_bid as $bid){
-                    $bid->status='lost';
-                    $bid->save();
-                    echo "Auction ID: ".$auction_data->id." has been lost by ".$bid->user_id."\n"; //replace with sms topic
+                $rem_bid = Bid::where('auction_id',$auction->id)->where('bid_id','<>',$max_bid->bid_id)->orderBy('bid_qty','desc')->orderBy('bid_time','asc')->get();
+                foreach($rem_bid as $bid){    
+                    if($bid->bid_qty<=$rem_quant){
+                        $bid->status='won';
+                        $bid->save();
+                        $msg = "Dear User, you were declared winner in auction ".$auction->auction_id." Click here to contact Seller ".$auction->seller_name." for further steps.";
+                        $this->send_msg($mobile_num,$msg);
+                        $rem_quant = $rem_quant - $bid->bid_qty;
+                    }else{
+                        $bid->status='lost';
+                        $bid->save();
+                        $msg = "Dear User, auction - ".$auction->auction_id." has ended. You have lost the auction. -ARYAMKP";
+                        $this->send_msg($mobile_num,$msg);
+                    }
                 }
             }
 
